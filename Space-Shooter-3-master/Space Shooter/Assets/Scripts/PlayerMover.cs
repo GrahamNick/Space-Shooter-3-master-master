05f37e2136e35b77e6f561eb996ac2cf052039f0
GrahamNick <37307748+GrahamNick@users.noreply.github.com>
2018-12-12 18:28:21 -0500

    first

---
+++ Space-Shooter-3-master/Space Shooter/Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, yMin, yMax, zMin, zMax;
+}
+public class PlayerMover : MonoBehaviour {
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+	public GameObject shot;
+	public Transform shotSpawn1;
+	public Transform shotSpawn2;
+	public Transform shotSpawn3;
+	public float fireDelta = 0.5f;
+	private float nextFire = 0.5f;
+	void Update() {
+
+		if (Input.GetButton("Fire1") && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireDelta;
+			Instantiate (
+				shot,
+				shotSpawn1.position,
+				shotSpawn1.rotation
+			);// as GameObject;
+			Instantiate (
+				shot,
+				shotSpawn2.position,
+				shotSpawn2.rotation
+			);// as GameObject;
+			Instantiate (
+				shot,
+				shotSpawn3.position,
+				shotSpawn3.rotation
+			);// as GameObject;
+			GetComponent<AudioSource> ().Play();
+		}
+
+	}
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0.0f);
+		GetComponent<Rigidbody> ().velocity = movement * speed;
+
+		GetComponent<Rigidbody> ().position = new Vector3 (
+			Mathf.Clamp (GetComponent<Rigidbody> ().position.x, boundary.xMin, boundary.xMax),
+			Mathf.Clamp (GetComponent<Rigidbody> ().position.y, boundary.yMin, boundary.yMax),
+			0.0f
+
+		);
+
+		GetComponent<Rigidbody> ().rotation = Quaternion.Euler (GetComponent<Rigidbody> ().velocity.y * -tilt, GetComponent<Rigidbody> ().velocity.x * tilt, 0.0f);
+	}
+}
